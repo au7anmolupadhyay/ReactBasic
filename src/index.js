f6ace53a72aea93e48f5bf8986283810da2ea261
Anmol Upadhyay <anmolupadhau7@gmail.com>
2024-12-02 18:00:39 +0530

    updated index.js

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import './style.css';
 import Header from './components/header';
 import Item from './components/items';
 import Button from './components/button';
+import Counter from './components/counter';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <div className='card'>
-        {/* <Counter/> */}
+        <Counter/>
         <Header heading = "TODO List"/>
         <Item completed = "true" text = "Eat"/>
         <Item text = "Play Cricket"/>
