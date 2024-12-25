d7cf0f13534673c7f993a67e78bac3086eebf186
Anmol Upadhyay <anmolupadhau7@gmail.com>
2024-12-25 11:06:13 +0530

    updated

--- api_fetch/api_fetch/src/app.js
+++ api_fetch/api_fetch/src/app.js
@@ -1,15 +1,18 @@
 import {useState, useEffect} from 'react';
 import { getPosts } from "./api";
+import PostCard from './components/PostCard';
+import "./app.css"
 
 function App(){
 
-    const [data, setData] = useState(null);
+    const [data, setData] = useState(null);   
 
-    useEffect(() =>{
+    useEffect(() =>{   //why use effect, because we want to see when it mounts
         getPosts().then((posts) => setData(posts));
     }, []);
+
     return (<div className="App">{
-        data ? data.map((e) => <li>{e.title}</li>) : <></>
+        data ? data.map((e) => <PostCard title={e.title} body={e.body}/>) : <></>
     }</div>)
 }
 
